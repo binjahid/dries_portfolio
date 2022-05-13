a9b0e5038ab9a44ad9573f4129aa8c87c27c0633
binjahid <muhammadjahidbinmutalib@gmail.com>
2022-05-14 02:20:39 +0600

    home done

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,24 +1,71 @@
+import { useTypewriter, Typewriter } from "react-simple-typewriter";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { AiTwotonePhone } from "react-icons/ai";
+
 import sampleProfile from "assets/images/sampleImg.png";
-import Typist from "react-typist";
+import SocialLink from "common/SocialLink";
 
 const HomeComponent = () => {
+  const { text } = useTypewriter({
+    words: ["Graphic designer", "Student"],
+    loop: 0,
+    cursorStyle: "_",
+  });
+
   return (
     <section className="w-full h-screen bg-primary flex justify-center items-center">
-      <div className="">
+      <div className="flex flex-col justify-center items-center">
         <div className="h-48 w-48 rounded-full">
           <img src={sampleProfile} alt="profile" className="h-full w-full" />
         </div>
         <div className="text-center mt-4">
-          <Typist>
-            <span className="my-custom-class"> First Sentence </span>
-            <br />
-            <div className="container">
-              <p> This will be animated after first sentence is complete </p>
-              {/* <MyComponent prop1="val1"> More text. </MyComponent> */}
-            </div>
-            Final sentence
-          </Typist>
+          <h2 className="text-white font-courgette lg:text-6xl md:text-5xl text-3xl ">
+            <span className="block sm:inline">I'm Dries Van Dessel </span>
+
+            <span className="text-secondary">
+              <Typewriter
+                words={["Graphic designer", "Student"]}
+                loop={0}
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={700}
+              />
+            </span>
+          </h2>
         </div>
+        <div className="flex items-center py-16">
+          <SocialLink
+            icon={<FaFacebook />}
+            link="https://www.facebook.com/dries.vandessel"
+            color={`hover:text-facebook`}
+          />
+          <SocialLink
+            icon={<FaInstagram />}
+            link="https://www.instagram.com/driesvandessel/"
+            color={`hover:text-instagram`}
+          />
+          <SocialLink
+            icon={<FaTwitter />}
+            link="https://twitter.com/driesvandessel"
+            color={`hover:text-twitter`}
+          />
+          <SocialLink
+            icon={<SiGmail />}
+            link="mailto:youmail@gmail.com"
+            color={"hover:text-gmail"}
+          />
+          <SocialLink
+            icon={<AiTwotonePhone />}
+            link="tel:+316-837-8982"
+            color={"hover:text-green-500"}
+          />
+        </div>
+        <button className="py-2 px-8 border border-secondary rounded-3xl bg-secondary  text-white font-bold hover:text-secondary hover:bg-transparent transform hover:scale-110 duration-500">
+          More About Me
+        </button>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     extend: {
       colors: {
         primary: "#353353",
+        secondary: "#ff4c60",
+        facebook: "#3b5998",
+        gmail: "#d44638",
+        twitter: "#1da1f2",
+        instagram: "#e1306c",
+        phone: "#f5f5f5",
       },
       backgroundImage: {
         "home-bg": "url('assets/images/homeBg.jpg')",
